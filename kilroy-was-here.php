a696913c665cfc99b25dc4c8657c90ed05145923
walterebert <walterebert@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-09 21:18:31 +0000

    Added uninstall.php & cleanup
    git-svn-id: https://plugins.svn.wordpress.org/kilroy-was-here/trunk@694808 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kilroy-was-here.php
+++ kilroy-was-here.php
@@ -3,10 +3,11 @@
 Plugin Name: Kilroy was here
 Plugin URI: http://wordpress.org/extend/plugins/kilroy-was-here/
 Description: Adds a text tag to the footer of posts & pages
-Version: 1.0.3
+Version: 1.0.4
 Author: Walter Ebert 
 Author URI: http://walterebert.com
 License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 class kilroywashere
@@ -23,7 +24,6 @@ class kilroywashere
 		add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array ( $this, 'action_link' ) );
 
 		register_activation_hook( __FILE__, array( $this, 'install' ) );
-		register_deactivation_hook( __FILE__, array( $this, 'uninstall' ) );
 	}
 
 	/**
@@ -39,7 +39,7 @@ class kilroywashere
 	 */
 	public function admin_menu()
 	{
-		add_options_page( 'Kilroy was here', 'Kilroy was here', 'administrator', __FILE__, array( $this, 'options' ) );
+		add_options_page( 'Kilroy was here', 'Kilroy was here', 'administrator', basename( __DIR__ ), array( $this, 'options' ) );
 	}
 
 	/**
@@ -47,9 +47,6 @@ class kilroywashere
 	 */
 	public function options()
 	{
-		if ( !current_user_can( 'manage_options' ) ) {
-			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-		}
 		?>
 		<div class="wrap">
 			<div id="icon-options-general" class="icon32">
@@ -78,8 +75,8 @@ class kilroywashere
 	public function action_link( $links )
 	{
 		return array_merge(
-			$links,
-			array( '<a href="options-general.php?page=' . plugin_basename( __FILE__ ) . '">' . __( 'Settings' ) . '</a>' )
+			array( '<a href="options-general.php?page=' . basename( __DIR__ ) . '">' . __( 'Settings' ) . '</a>' ),
+			$links
 		);
 	}
 
@@ -101,15 +98,6 @@ class kilroywashere
 	{
 		add_option( 'kilroywashere-content', "`     ,,,\n     (o o)\n--ooO-(_)-Ooo---" );
 	}
-
-	/**
-	 * Run on deactivation
-	 */
-	public function uninstall()
-	{
-		delete_option( 'kilroywashere-content' );
-	}
-
 }
 
 // Create instance
---
+++ uninstall.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Code used when the plugin is removed (not just deactivated but actively deleted through the WordPress Admin).
+ */
+
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit;
+}
+
+delete_option( 'kilroywashere-content' );
